45bc4232e21d1e6570b92b057b7d0e88e01354d4
VernPherus <vernpherus@gmail.com>
2025-12-10 10:39:39 +0800

    setup api routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,14 @@
 <?php
 
-use App\Http\Controllers\DisbursementController;
-use App\Http\Controllers\FundAccountController;
-use App\Http\Controllers\PayeeAccountController;
-
 use App\Http\Controllers\Admin\DisbursementAdminController;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::resource('disbursementadmin', DisbursementAdminController::class);
 
 Route::get('/', function(){
     return redirect()->route('disbursementadmin.index');
 });
-
-// Standard CRUD
-Route::resource('disbursements', DisbursementController::class);
-Route::resource('funds', FundAccountController::class);
-Route::resource('payees', PayeeAccountController::class);
-
-// Admin 
-Route::resource('disbursementadmin', DisbursementAdminController::class);
-
-//
-Route::patch('/disbursements/{id}/approve', [DisbusrsementController::class, 'approve'])->name('disbursements.approve');
 
